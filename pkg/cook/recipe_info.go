1c45c7576f7d37cb99df5f0a3f6d54ea06e87616
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-04-12 19:15:24 -0700

    Add ImagePath

--- pkg/cook/recipe_info.go
+++ pkg/cook/recipe_info.go
@@ -1,12 +1,14 @@
 package cook
 
 import (
-  "fmt"
+  "os"
+  "errors"
   "strings"
   "path/filepath"
 )
 
 type RecipeDocumentationInfo struct {
+  ImagePath string
   RecipePath string
   RecipeName string
 }
@@ -17,6 +19,15 @@ func ParseRecipeInformation(recipePath string) (RecipeDocumentationInfo, error) 
   recipeDocInfo.RecipePath = recipePath
   fileName := filepath.Base(recipePath)
   recipeDocInfo.RecipeName = strings.TrimSuffix(fileName, filepath.Ext(fileName))
-  fmt.Println(recipeDocInfo.RecipeName)
+  path := filepath.Dir(recipePath)
+  imagePath := filepath.Join(path, recipeDocInfo.RecipeName) + ".jpg"
+  _, err = os.Stat(imagePath)
+  if errors.Is(err, os.ErrNotExist) {
+    imagePath = filepath.Join(path, recipeDocInfo.RecipeName) + ".png"
+  }
+  _, err = os.Stat(imagePath)
+  if !errors.Is(err, os.ErrNotExist) {
+    recipeDocInfo.ImagePath = imagePath
+  }
   return recipeDocInfo, err
 }
