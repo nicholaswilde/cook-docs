db6be1ec0f42fec112c4e6ff857d4c5ef163a18b
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-04-16 20:31:59 -0700

    Replace tabs with spaces

--- pkg/cook/recipe_finder.go
+++ pkg/cook/recipe_finder.go
@@ -6,16 +6,16 @@ import (
 )
 
 func FindRecipeDirectories(recipeSearchRoot string) ([]string, error) {
-	recipeDirs := make([]string, 0)
-	err := filepath.Walk(recipeSearchRoot, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if filepath.Ext(path) == ".cook" {
-			recipeDirs = append(recipeDirs, path)
-		}
-		return nil
-	})
+  recipeDirs := make([]string, 0)
+  err := filepath.Walk(recipeSearchRoot, func(path string, info os.FileInfo, err error) error {
+    if err != nil {
+      return err
+    }
+    if filepath.Ext(path) == ".cook" {
+      recipeDirs = append(recipeDirs, path)
+    }
+    return nil
+  })
 
-	return recipeDirs, err
+  return recipeDirs, err
 }
