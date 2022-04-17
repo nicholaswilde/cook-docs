84bf01562457dbe7dcb8b3a01851266605ecb97e
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2022-04-16 20:40:10 -0700

    Replace tabs with spaces

--- cmd/cook-docs/main.go
+++ cmd/cook-docs/main.go
@@ -19,12 +19,12 @@ func retrieveInfoAndPrintDocumentation(recipeSearchRoot string, recipePath strin
 
   recipeInfo := cook.ParseRecipeInformation(recipePath)
 
-	recipeData, err := cooklang.ParseFile(recipeInfo.RecipePath)
+  recipeData, err := cooklang.ParseFile(recipeInfo.RecipePath)
 
-	if err != nil {
+  if err != nil {
     log.Warnf("Error parsing file for recipe %s, skipping: %s", recipeInfo.RecipePath, err)
-		return
-	}
+    return
+  }
 
   recipeData = cook.MergeRecipeData(recipeInfo, recipeData)
 
@@ -32,28 +32,28 @@ func retrieveInfoAndPrintDocumentation(recipeSearchRoot string, recipePath strin
 }
 
 func cookDocs(_ *cobra.Command, _ []string) {
-	initializeCli()
-	recipeSearchRoot := viper.GetString("recipe-search-root")
-	var fullRecipeSearchRoot string
-	if path.IsAbs(recipeSearchRoot) {
-		fullRecipeSearchRoot = recipeSearchRoot
-	} else {
-		cwd, err := os.Getwd()
-		if err != nil {
-			log.Warnf("Error getting working directory: %s", err)
-			return
-		}
-		fullRecipeSearchRoot = path.Join(cwd, recipeSearchRoot)
-	}
+  initializeCli()
+  recipeSearchRoot := viper.GetString("recipe-search-root")
+  var fullRecipeSearchRoot string
+  if path.IsAbs(recipeSearchRoot) {
+    fullRecipeSearchRoot = recipeSearchRoot
+  } else {
+    cwd, err := os.Getwd()
+    if err != nil {
+      log.Warnf("Error getting working directory: %s", err)
+      return
+    }
+    fullRecipeSearchRoot = path.Join(cwd, recipeSearchRoot)
+  }
   recipeDirs, err := cook.FindRecipeDirectories(fullRecipeSearchRoot)
   if err != nil {
-		log.Errorf("Error finding recipe directories: %s", err)
-		os.Exit(1)
-	}
+    log.Errorf("Error finding recipe directories: %s", err)
+    os.Exit(1)
+  }
   log.Infof("Found recipes [%s]", strings.Join(recipeDirs, ", "))
 
   templateFiles := viper.GetStringSlice("template-files")
-	log.Debugf("Rendering from optional template files [%s]", strings.Join(templateFiles, ", "))
+  log.Debugf("Rendering from optional template files [%s]", strings.Join(templateFiles, ", "))
 
   dryRun := viper.GetBool("dry-run")
   waitGroup := sync.WaitGroup{}
@@ -62,7 +62,7 @@ func cookDocs(_ *cobra.Command, _ []string) {
     waitGroup.Add(1)
 
     // On dry runs all output goes to stdout, and so as to not jumble things, generate serially
-		if dryRun {
+    if dryRun {
       retrieveInfoAndPrintDocumentation(fullRecipeSearchRoot, r, templateFiles, &waitGroup, dryRun)
     } else {
       go retrieveInfoAndPrintDocumentation(fullRecipeSearchRoot, r, templateFiles, &waitGroup, dryRun)
